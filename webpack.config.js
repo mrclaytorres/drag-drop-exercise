b81ddff90f9eb404f6ee4b37a1ee7e976327d9ed
Algen <adelacruz@innovuze.com>
2022-09-26 13:50:01 +0800

    Setup webpack development server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,26 @@ module.exports = {
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
+  },
+  devServer: {
+    open: true,
+    static: {
+      directory: path.join(__dirname, '/')
+    }
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   }
 };
